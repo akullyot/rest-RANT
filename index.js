d39ec4f0745c0492ea6ddd66e2a25badb0e8caf5
akullyot <akullyot@gmail.com>
2023-08-29 02:54:52 -0500

    initialized express and dotenv, and added a home and catch all page route

---
+++ index.js
@@ -0,0 +1,20 @@
+//Initalize express and include environment variables
+require('dotenv').config();
+const express = require('express');
+const app     = express();
+
+
+//Purpose: Homepage
+app.get('/', (request,response) =>
+{
+    response.send('Hello World')
+});
+
+//Purpose: Catch all Page
+app.get('*', (request,response) =>
+{
+    //chain together the sent HTML and the HTTP status
+    response.status(404).send('<h1> 404 Page not Found </h1>')
+
+})
+app.listen(process.env.PORT)
